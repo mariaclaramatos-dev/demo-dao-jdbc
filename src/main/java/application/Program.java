13211f964a9cee68ded9cbf6933f40af84110a19
Maria Clara Matos <mariaclaramatos086@gmail.com>
2026-07-09 09:40:57 -0300

    delete implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -7,9 +7,11 @@ import model.entities.Seller;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao(); // Injeção de depensência
 
         System.out.println("=== TEST 1: seller findById ===");
@@ -41,5 +43,12 @@ public class Program {
         seller.setName("Martha Waine");
         sellerDao.update(seller);
         System.out.println("Update completed");
+
+        System.out.println("\n=== TEST 5: seller delete ===");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+        sc.close();
     }
 }
